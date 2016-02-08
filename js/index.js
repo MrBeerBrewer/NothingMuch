77d9b7babfd0bb9f11a5db04f851d25c2cc1b584
Santosh J <Santosh J>
2016-02-08 22:20:47 +0530

    New

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $('document').ready(function() {
-	var options = { videoId: 'aVV9z7pWohc', start: 3 };
-	$('#wrapper').tubular(options);
+	//var options = { videoId: 'aVV9z7pWohc', start: 3 };
+	//$('#wrapper').tubular(options);
 	// f-UGhWj1xww cool sepia hd
 	// 49SKbS7Xwf4 beautiful barn sepia
 	//Gauqme-2SbI water
