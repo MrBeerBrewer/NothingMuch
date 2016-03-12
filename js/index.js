10434bc2d4a69664decf4e280e03ba6840a97406
SantoshJ <santosh@nothingmuch.in>
2016-03-12 17:12:57 +0530

    New

--- js/index.js
+++
@@ -1,9 +0,0 @@
-$('document').ready(function() {
-	var options = { videoId: 'aVV9z7pWohc', start: 3 };
-	$('#wrapper').tubular(options);
-	// f-UGhWj1xww cool sepia hd
-	// 49SKbS7Xwf4 beautiful barn sepia
-	//Gauqme-2SbI water
-	//9jFVf-xcT5U nice blu
-
-});
